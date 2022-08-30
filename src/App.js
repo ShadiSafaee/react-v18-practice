2312046f62265718ef607c7e6ccfbcbd2415d4e3
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-08-30 22:45:46 +0100

    default props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
       <h1>Random Number</h1>
       <br />
       <Counter count={5} />
-      <Counter count={10} />
+      <Counter myArray={[1, 2, 3]} firstName="Shadi" />
       <Counter count={20}>This is my counter</Counter>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const Counter = (props) => {
   console.log(props);
-  return <p>Counter:{props.count}</p>;
+  return (
+    <div>
+      <p>Counter:{props.count || 90}</p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
+
+// Counter.defaultProps = {
+//   count: 25,
+// };
 
 export default Counter;
